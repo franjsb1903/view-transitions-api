34384353dcfbec538903704b8226d2fa62984ca3
Fran Saa <fran.saa@interacso.com>
2023-07-08 14:33:57 +0200

    Finish transition api formation

---
+++ src/data/books.js
@@ -0,0 +1,34 @@
+export const books = [
+	{
+		id: 'clean-code',
+		title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+		opinion:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet aliquet dui. Mauris faucibus ante at congue posuere. Ut placerat scelerisque feugiat. Proin imperdiet sem nec mi dapibus, in rutrum velit pretium. Curabitur id turpis erat. Maecenas efficitur dictum metus et dignissim. Fusce fringilla, nibh eu gravida scelerisque, dolor diam semper nulla, nec fermentum ipsum elit sed augue. Suspendisse semper ultrices metus, vitae lacinia odio laoreet ut. Nunc venenatis volutpat est ac elementum. Aenean dictum faucibus mollis. Quisque venenatis quam ut diam mattis mattis. Nulla suscipit ligula vitae leo mattis, in consectetur sapien vulputate.',
+		author: 'Pepe'
+	},
+	{
+		id: 'pragmatic-programmer',
+		title: 'The Pragmatic Programmer: From Journeyman to Master',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+		opinion:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet aliquet dui. Mauris faucibus ante at congue posuere. Ut placerat scelerisque feugiat. Proin imperdiet sem nec mi dapibus, in rutrum velit pretium. Curabitur id turpis erat. Maecenas efficitur dictum metus et dignissim. Fusce fringilla, nibh eu gravida scelerisque, dolor diam semper nulla, nec fermentum ipsum elit sed augue. Suspendisse semper ultrices metus, vitae lacinia odio laoreet ut. Nunc venenatis volutpat est ac elementum. Aenean dictum faucibus mollis. Quisque venenatis quam ut diam mattis mattis. Nulla suscipit ligula vitae leo mattis, in consectetur sapien vulputate.',
+		author: 'Pepe'
+	},
+	{
+		id: 'eloquent-javascript',
+		title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg',
+		opinion:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet aliquet dui. Mauris faucibus ante at congue posuere. Ut placerat scelerisque feugiat. Proin imperdiet sem nec mi dapibus, in rutrum velit pretium. Curabitur id turpis erat. Maecenas efficitur dictum metus et dignissim. Fusce fringilla, nibh eu gravida scelerisque, dolor diam semper nulla, nec fermentum ipsum elit sed augue. Suspendisse semper ultrices metus, vitae lacinia odio laoreet ut. Nunc venenatis volutpat est ac elementum. Aenean dictum faucibus mollis. Quisque venenatis quam ut diam mattis mattis. Nulla suscipit ligula vitae leo mattis, in consectetur sapien vulputate.',
+		author: 'Pepe'
+	},
+	{
+		id: 'coders-at-work',
+		title: 'Coders at Work: Reflections on the Craft of Programming',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1349026758i/6713575.jpg',
+		opinion:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet aliquet dui. Mauris faucibus ante at congue posuere. Ut placerat scelerisque feugiat. Proin imperdiet sem nec mi dapibus, in rutrum velit pretium. Curabitur id turpis erat. Maecenas efficitur dictum metus et dignissim. Fusce fringilla, nibh eu gravida scelerisque, dolor diam semper nulla, nec fermentum ipsum elit sed augue. Suspendisse semper ultrices metus, vitae lacinia odio laoreet ut. Nunc venenatis volutpat est ac elementum. Aenean dictum faucibus mollis. Quisque venenatis quam ut diam mattis mattis. Nulla suscipit ligula vitae leo mattis, in consectetur sapien vulputate.',
+		author: 'Pepe'
+	}
+]
